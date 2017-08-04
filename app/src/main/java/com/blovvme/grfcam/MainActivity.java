c86239e2ddbd5f99b7f2dcdf965b96b3fe8ac11e
German Rodriguez <german_rodz@hotmail.com>
2017-08-04 11:22:44 -0400

    Initial commit

---
+++ app/src/main/java/com/blovvme/grfcam/MainActivity.java
@@ -0,0 +1,41 @@
+package com.blovvme.grfcam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import  android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 2;
+    ImageView img;
+    Bitmap btnC;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.img);
+    }
+
+    public void goToCamera(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(photo);
+        }
+    }
+}
